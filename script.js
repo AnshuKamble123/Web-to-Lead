88cfe395f33e5ffb4cbc17cdbe269e3d0606dcf3
AnshuKamble123 <102008301+AnshuKamble123@users.noreply.github.com>
2024-01-04 17:47:44 +0530

    recaptha validations

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-function beforesubmit(){
+    let capthachecked = false;
 
-let outputdate = document.querySelector(".outputdate");
-let inputdate = document.querySelector(".inputdate");
-console.log("inputdate value: ", inputdate.value); //string --> date(en_IN)
-let formattedDate = new Date(inputdate.value).toLocaleDateString("en_IN");
-outputdate.value = formattedDate;
-}
+    function beforesubmit(event){
+    if(capthachecked){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate value: ", inputdate.value); //string --> date(en_IN)
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en_IN");
+    outputdate.value = formattedDate;
+    }else{
+        alert("please check the reCAPTHA box to submit the lead");
+        event.preventDefault();
+    }
+
+    }
 
 
 
- function timestamp() { 
-    var response = document.getElementById("g-recaptcha-response"); 
-    if (response == null || response.value.trim() == "") {
-        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-        elems["ts"] = JSON.stringify(new Date().getTime());
-        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    function timestamp() { 
+        var response = document.getElementById("g-recaptcha-response"); 
+        if (response == null || response.value.trim() == "") {
+            var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+            elems["ts"] = JSON.stringify(new Date().getTime());
+            document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+        } 
     } 
-} 
-setInterval(timestamp, 500); 
+    setInterval(timestamp, 500); 
+
+    function capthasuccess(){
+        capthachecked=true;
+    }
